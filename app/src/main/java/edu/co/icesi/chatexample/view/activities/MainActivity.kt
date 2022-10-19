1f810c5edc64e8536c739aeb2bcd7dd062efe5f7
Domiciano Rincón <domiciano.rincon@correounivalle.edu.co>
2022-10-18 20:57:39 -0500

    Semana 12

--- app/src/main/java/edu/co/icesi/chatexample/view/activities/MainActivity.kt
+++ app/src/main/java/edu/co/icesi/chatexample/view/activities/MainActivity.kt
@@ -2,9 +2,11 @@ package edu.co.icesi.chatexample.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import edu.co.icesi.chatexample.databinding.ActivityMainBinding
 import edu.co.icesi.chatexample.view.ChatAdapter
+import edu.co.icesi.chatexample.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,11 +14,16 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val viewModel:MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.chatRV.adapter = ChatAdapter()
         binding.chatRV.layoutManager = LinearLayoutManager(this)
         binding.chatRV.setHasFixedSize(true)
+
+        viewModel.subscribeToMessages()
+
     }
 }
--- app/src/main/java/edu/co/icesi/chatexample/viewmodel/MainViewModel.kt
+++ app/src/main/java/edu/co/icesi/chatexample/viewmodel/MainViewModel.kt
@@ -1,9 +1,26 @@
 package edu.co.icesi.chatexample.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 
 class MainViewModel:ViewModel() {
 
+    fun subscribeToMessages(){
+        Firebase.firestore
+            .collection("users")
+            .addSnapshotListener{data, e ->
 
-
+                for(docChanges in data?.documentChanges!!){
+                    val user = docChanges.document.toObject(User::class.java)
+                    Log.e(">>>","${user.id} ${user.name} ${docChanges.type.name}")
+                }
+            }
+    }
 }
+data class User(
+    val id:String = "",
+    val name:String = ""
+)
